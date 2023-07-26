66f95f3e0cc4818e0092a75141b0cdd744cdc044
Aaditya Suri <aadityasuri1@gmail.com>
2023-07-25 21:47:25 -0700

    added other positions noise, this might actually work lmao

--- Model/Gesture_classification.py
+++ Model/Gesture_classification.py
@@ -2,17 +2,21 @@ import torch
 import torch.nn as nn
 
 
-class GestureFNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
+class GestureFNN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim_1, hidden_dim_2, output_dim):
         super(GestureFNN, self).__init__()
         # Linear function 1: 126 --> 84
-        self.fc1 = torch.nn.Linear(input_dim, hidden_dim)
+        self.fc1 = torch.nn.Linear(input_dim, hidden_dim_1)
 
         # Non-linearity 1
         self.sigmoid = torch.nn.Sigmoid()
 
-        # Linear function 2: 84 --> 3
-        self.fc2 = torch.nn.Linear(hidden_dim, output_dim)
+        # Linear function 2: 84 --> 2
+        self.fc2 = torch.nn.Linear(hidden_dim_1, hidden_dim_2)
+
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.fc3 = torch.nn.Linear(hidden_dim_2, output_dim)
 
     def forward(self, x):
         # Linear function 1
@@ -23,5 +27,9 @@ class GestureFNN(nn.Module):
 
         # Linear function 2 (readout)
         out = self.fc2(out)
+
+        out = self.sigmoid(out)
+
+        out = self.fc3(out)
         return out
 
